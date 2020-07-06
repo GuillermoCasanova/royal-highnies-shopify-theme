04ffd1b268ee13ea08f4691f0a2d2b1eccad2f65
Guillermo Casanova <Casanova@MacBook-Pro.local>
2020-07-06 15:46:18 -0400

    + Implementing animated string UX for tablets and implemetning resize catching for reinit

--- src/scripts/sections/about-highnies.js
+++ src/scripts/sections/about-highnies.js
@@ -17,6 +17,11 @@ theme.AboutHighnies = (function() {
       },
       init: function() {
 
+        if(this.scrollMagicCtrl) {
+          this.scrollMagicCtrl.destroy(true);
+          this.scene.destroy(true);
+        }
+
         var that = this; 
 
         var $string = $(this.selectors.stringPath); 
@@ -31,7 +36,6 @@ theme.AboutHighnies = (function() {
         this.stringTween = new TimelineMax()
                     .add(TweenMax.to(that.selectors.stringPath, 1, {strokeDashoffset: lineLength * 2 + 'px', ease:Linear.easeNone}));
 
-
         this.scene = new ScrollMagic.Scene({
           triggerElement: that.selectors.animationContainer, 
           duration: $('.about-us-narrative').height() - ($('.narrative-section--experience').height()/2),
@@ -39,10 +43,37 @@ theme.AboutHighnies = (function() {
         }).setTween(this.stringTween)
           .addTo(this.scrollMagicCtrl); 
 
-      }
 
+
+       var timerId = null;
+
+        var  throttle  =  function (func, delay) {
+          // If setTimeout is already scheduled, no need to do anything
+          if (timerId) {
+            return
+          }
+
+          // Schedule a setTimeout after delay seconds
+          timerId  =  setTimeout(function () {
+            func()
+            
+            // Once setTimeout function execution is finished, timerId = undefined so that in <br>
+            // the next scroll event function execution can be scheduled by the setTimeout
+            timerId  =  undefined;
+          }, delay)
+        }
+
+
+         $(window).on('resize', function() {
+            throttle(function() {
+                that.init(); 
+            }, 1000)
+         });
+      }
     };
 
-    aboutPageAnimations.init(); 
+    if($('.about-us-narrative__string').length > 0)  {
+      aboutPageAnimations.init(); 
+    }
 
  })(); 
